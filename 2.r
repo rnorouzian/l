ebdbba08103b9cf82b98ac1b720b62e8cbd8cf36
izeh <31554814+izeh@users.noreply.github.com>
2017-09-07 14:34:35 -0500

    Update 2.r

--- 2.r
+++ 2.r
@@ -7,10 +7,10 @@ min.d = d.SE*qt(1e-5, df, ncp.min)  ;  max.d = d.SE*qt(0.99999, df, ncp.max)
   
 for(i in 1:length(d)){      
 H = curve(dt(x*sqrt(N), df, d[i]*sqrt(N))*sqrt(N), min.d, max.d, n = 1e3, xlab = "Effect Size (Cohen's d)", 
-          ylab = NA, ty = "n", add = i!= 1, bty = "n", yaxt = "n", font = 2, font.lab = 2, xpd = TRUE)
+          ylab = NA, ty = "n", add = i!= 1, bty = "n", yaxt = "n", font = 2, font.lab = 2)
       
 polygon(H, col = adjustcolor(i, .7), border = NA)
-text(d[i], max(H$y), bquote(bolditalic(H[.(i-1)])), pos = 3)
+text(d[i], max(H$y), bquote(bolditalic(H[.(i-1)])), pos = 3, xpd = TRUE)
 axis(1, at = d[i], col = i, col.axis = i)
 segments(d[i], 0, d[i], max(H$y), lty = 3)
    }
