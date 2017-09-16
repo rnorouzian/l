74f3bbb0d686e15019bd2d50730e502832df7355
izeh <31554814+izeh@users.noreply.github.com>
2017-09-16 12:49:40 -0500

    Update 2.r

--- 2.r
+++ 2.r
@@ -5,7 +5,7 @@ Figure.2 = function(N, d){
   min.d = d.SE*qt(1e-5, df, ncp.min)  ;  max.d = d.SE*qt(0.99999, df, ncp.max)  
   
   for(i in 1:length(d)){      
-    H = curve(dt(x*sqrt(N), df, d[i]*sqrt(N))/d.SE, min.d, max.d, n = 1e3, xlab = "Effect Size (Cohen's d)", 
+    H = curve(dt(x*sqrt(N), df, d[i]*sqrt(N)), min.d, max.d, n = 1e3, xlab = "Effect Size (Cohen's d)", 
               ylab = NA, ty = "n", add = i!= 1, bty = "n", yaxt = "n", font.lab = 2, yaxs = "i")
     
     polygon(H, col = adjustcolor(i, .7), border = NA)
